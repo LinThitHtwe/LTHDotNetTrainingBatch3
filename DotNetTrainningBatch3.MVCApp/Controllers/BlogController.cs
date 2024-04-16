d351b6b0849a4c5e1cfac1aaa9c515cffc548427
LinThitHtwe <linthit2745@gmail.com>
2024-04-16 23:47:21 +0630

    [add] Create,Update,Delete in MVC

--- DotNetTrainningBatch3.MVCApp/Controllers/BlogController.cs
+++ DotNetTrainningBatch3.MVCApp/Controllers/BlogController.cs
@@ -21,5 +21,63 @@ namespace DotNetTrainningBatch3.MVCApp.Controllers
             List<BlogModel> blogs = _appDbContext.Blogs.ToList();
             return View("BlogIndex",blogs);
         }
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(string id)
+        {
+            var blog = _appDbContext.Blogs.FirstOrDefault(b => b.Id == id);
+            if(blog is null)
+            {
+                return Redirect("/blog");
+            }
+            return View("BlogEdit",blog);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogModel blogModel)
+        {
+            _appDbContext.Blogs.Add(blogModel);
+            _appDbContext.SaveChanges();
+            return Redirect("/blog");
+        }
+
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(string id,BlogModel blogModel)
+        {
+
+            var blog = _appDbContext.Blogs.FirstOrDefault(b => b.Id == id);
+
+            if(blog is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            blog.Title = blogModel.Title;
+            blog.Author = blogModel.Author;
+
+            
+            _appDbContext.SaveChanges();
+            return Redirect("/blog");
+        }
+
+        [ActionName("Delete")]
+        public IActionResult BlogDelete(string id)
+        {
+            var blog = _appDbContext.Blogs.FirstOrDefault(b => b.Id == id);
+            if (blog is null)
+            {
+                return Redirect("/blog");
+            }
+            _appDbContext.Remove(blog);
+            _appDbContext.SaveChanges();
+            return Redirect("/blog");
+        }
+
     }
 }
