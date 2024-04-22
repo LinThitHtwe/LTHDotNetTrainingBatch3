97a01570715ba9272baca2bafc2d7d8dd7a40758
LinThitHtwe <linthit2745@gmail.com>
2024-04-22 16:30:19 +0630

    [add] ApexChart Pie chart in MVC

---
+++ DotNetTrainningBatch3.MVCApp/Controllers/ApexChartController.cs
@@ -0,0 +1,19 @@
+﻿using DotNetTrainningBatch3.MVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainningBatch3.MVCApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            ApexChartResponseModel apexChartResponseModel = new()
+            {
+                Series = new List<int> { 10, 2, 5 },
+                Labels = new List<string> { "KFC", "LOL", "HELP" }
+            };
+
+            return View(apexChartResponseModel);
+        }
+    }
+}
---
+++ DotNetTrainningBatch3.MVCApp/Models/ApexChartResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetTrainningBatch3.MVCApp.Models
+{
+    public class ApexChartResponseModel
+    {
+        public List<int> Series { get; set; }
+        public List<string> Labels { get; set; }
+    }
+}
