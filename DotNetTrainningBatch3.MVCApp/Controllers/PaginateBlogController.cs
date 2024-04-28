ca50969c8a8d07222aa965075cf5a6d90fa1fab4
LinThitHtwe <linthit2745@gmail.com>
2024-04-28 10:33:09 +0630

    [add] MVC Pagination

---
+++ DotNetTrainningBatch3.MVCApp/Controllers/PaginateBlogController.cs
@@ -0,0 +1,36 @@
+﻿using DotNetTrainningBatch3.MVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainningBatch3.MVCApp.Controllers
+{
+    public class PaginateBlogController : Controller
+    {
+        private AppDbContext _appDbContext;
+
+        public PaginateBlogController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        [ActionName("Index")]
+        public IActionResult Index(int pageNo = 1, int pageSize = 10)
+        {
+            List<BlogModel> blogs = _appDbContext.Blogs
+                                .Skip((pageNo - 1) * pageSize)
+                                .Take(pageSize)
+                                .ToList();
+
+            int rowCount = _appDbContext.Blogs.Count();
+            int pageCount = (int)Math.Ceiling((double)rowCount / pageSize);
+
+            BlogPaginatedResponseModel responseModel = new()
+            {
+                Data = blogs,
+                PageCount = pageCount,
+                PageNo = pageNo,
+                PageSize = pageSize,
+            };
+            return View("BlogIndex", responseModel);
+        }
+    }
+}
